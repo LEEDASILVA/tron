788e363be940590ae64d851c06fb8962cf09fbdc
lee <lee-dasilva@hotmail.com>
2020-06-05 14:06:41 +0100

    version 0.1: adding correct alley verification

--- ai/LEEDASILVA.js
+++ ai/LEEDASILVA.js
@@ -2,32 +2,21 @@
  * functions given to the students
  ********************************/
 const SIZE = 100
-const MAP = new Int8Array(SIZE * SIZE) // State of the Map
-const isFree = ({ x, y }) => MAP[y * SIZE + x] === 0 // 0 = block free
-const isOccupied = ({ x, y }) => MAP[y * SIZE + x] === 1 // 1 = block occupied
-// `inBounds` check if our coord (n) is an existing index in our MAP
+const MAP = new Int8Array(SIZE * SIZE)
+const isFree = ({ x, y }) => MAP[y * SIZE + x] === 0
+const isOccupied = ({ x, y }) => MAP[y * SIZE + x] === 1
 const inBounds = (n) => n < SIZE && n >= 0
-// `isInBounds` check that properties x and y of our argument are both in bounds
 const isInBounds = ({ x, y }) => inBounds(x) && inBounds(y)
-// `pickRandom` Get a random element from an array
 const pickRandom = (arr) => arr[Math.floor(Math.random() * arr.length)]
 
 /***********
  * My functions
  ************/
-// // for now this function will search for enemies
-// // the goal is to KILL THEM ALL :D
-// // find the enemy that is mos close to has and focus on that one
-// const getEnemies = (players) =>
-//   players.filter((pl) => !/LEEDASILVA+/g.test(pl.name))
-
-// const attackMode = (state) => {
-//   let targets = getEnemies(state.players)
-// }
 const symmetric12 = (x, y, direction) => {
   if (direction === 1 || direction === 2) {
     let up = path(x, y - 1, 0, 0)
-    let left = path(x - 1, y, 3, 0)               /////////// isAlley use it to final version
+    let left = path(x - 1, y, 3, 0) /////////// isAlley use it to final version
+    // console.log('up left', up, left)
     return up > left
       ? 1
       : up === left && direction === 1
@@ -41,6 +30,7 @@ const symmetric03 = (x, y, direction) => {
   if (direction === 3 || direction === 0) {
     let right = path(x + 1, y, 1, 0)
     let down = path(x, y + 1, 2, 0)
+    // console.log('down right', down, right)
     return down > right
       ? 3
       : down === right && direction === 3
@@ -54,6 +44,7 @@ const symmetric01 = (x, y, direction) => {
   if (direction === 0 || direction === 1) {
     let down = path(x, y + 1, 2, 0)
     let left = path(x - 1, y, 3, 0)
+    // console.log('down left', down, left)
     return down > left
       ? 1
       : down === left && direction === 1
@@ -67,6 +58,7 @@ const symmetric23 = (x, y, direction) => {
   if (direction === 2 || direction === 3) {
     let up = path(x, y - 1, 0, 0)
     let right = path(x + 1, y, 1, 0)
+    // console.log('up right', up, right)
     return up > right
       ? 3
       : up === right && direction === 3
@@ -93,10 +85,7 @@ const findBestPath = (state) => {
       return state.player.coords[symmetric03(xad, yad, state.player.cardinal)]
     }
   }
-
   if (state.player.cardinal === 0 || state.player.cardinal === 1) {
-    // if it as a block on the symmetric position it must
-    // simulate the symmetric position and see witch path is the best
     let xad = state.player.x + 1
     let yad = state.player.y - 1
     if (!isFree({ x: xad, y: yad })) {
@@ -104,8 +93,6 @@ const findBestPath = (state) => {
     }
   }
   if (state.player.cardinal === 1 || state.player.cardinal === 2) {
-    // if it as a block on the symmetric position it must
-    // simulate the symmetric position and see witch path is the best
     let xad = state.player.x + 1
     let yad = state.player.y + 1
     if (!isFree({ x: xad, y: yad })) {
@@ -113,8 +100,6 @@ const findBestPath = (state) => {
     }
   }
   if (state.player.cardinal === 2 || state.player.cardinal === 3) {
-    // if it as a block on the symmetric position it must
-    // simulate the symmetric position and see witch path is the best
     let xad = state.player.x - 1
     let yad = state.player.y + 1
     if (!isFree({ x: xad, y: yad })) {
@@ -130,14 +115,14 @@ const findBestPath = (state) => {
 }
 
 const path = (x, y, car, count) => {
-  if (car === 0) {
+  if (car <= 0) {
     if (
       isAlley({ x: x + 1, y }) &&
-      isAlley({ x: x, y: y + 1 }) &&
+      isAlley({ x: x, y: y - 1 }) &&
       isAlley({ x: x - 1, y })
     )
-      return 0
-    return !isFree({ x, y }) || !inBounds(y)
+      return -1
+    return !isFree({ x, y }) || !isFree({ x, y: y - 1 }) || !inBounds(y - 1)
       ? count
       : path(x, y - 1, car, count + 1)
   }
@@ -145,32 +130,32 @@ const path = (x, y, car, count) => {
     if (
       isAlley({ x, y: y + 1 }) &&
       isAlley({ x, y: y - 1 }) &&
-      isAlley({ x: x - 1, y })
+      isAlley({ x: x + 1, y })
     )
-      return 0
-    return !isFree({ x, y }) || !inBounds(x)
+      return -1
+    return !isFree({ x, y }) || !isFree({ x: x + 1, y }) || !inBounds(x + 1)
       ? count
       : path(x + 1, y, car, count + 1)
   }
   if (car === 2) {
     if (
       isAlley({ x: x - 1, y }) &&
-      isAlley({ x, y: y - 1 }) &&
+      isAlley({ x, y: y + 1 }) &&
       isAlley({ x: x + 1, y })
     )
-      return 0
-    return !isFree({ x, y }) || !inBounds(y)
+      return -1
+    return !isFree({ x, y }) || !isFree({ x, y: y + 1 }) || !inBounds(y + 1)
       ? count
       : path(x, y + 1, car, count + 1)
   }
   if (car === 3) {
     if (
       isAlley({ x, y: y - 1 }) &&
-      isAlley({ x: x + 1, y }) &&
+      isAlley({ x: x - 1, y }) &&
       isAlley({ x, y: y + 1 })
     )
-      return 0
-    return !isFree({ x, y }) || !inBounds(x)
+      return -1
+    return !isFree({ x, y }) || !isFree({ x: x - 1, y }) || !inBounds(x - 1)
       ? count
       : path(x - 1, y, car, count + 1)
   }
@@ -184,13 +169,12 @@ const update = (state) => {
   // - attack mode that will search for the enemies and try to trap him
   //   the attack mode will be for the min of 2 players
 
+  // console.log(state.player)
+
   // return state.players.length <= 2 ? attackMode(state) : findBestPath(state)
   return findBestPath(state)
 }
 
-// This part of the code should be left untouch since it's initializing
-// and configuring communication of the web worker to the main page.
-// Only edit that if you know your shit
 addEventListener(
   'message',
   (self.init = (initEvent) => {
