51313749b95f19c83a19611094ee5550359301be
lee <lee-dasilva@hotmail.com>
2020-06-08 16:16:46 +0100

    clean code and final version

--- ai/LEEDASILVA.js
+++ ai/LEEDASILVA.js
@@ -12,188 +12,97 @@ const pickRandom = (arr) => arr[Math.floor(Math.random() * arr.length)]
 /***********
  * My functions // MUST REFACTOR THIS CODE !!!!!!!!!!!!!! VERY BAD!!!! BUT IT WORKS
  ************/
-const symmetric12 = (x, y, direction) => {
-  if (direction === 1 || direction === 2) {
-    let up = calDistance(x, y - 1, 0, 0)
-    let left = calDistance(x - 1, y, 3, 0)
-    let line =
-      direction === 1
-        ? calDistance(x, y - 1, direction, 0)
-        : calDistance(x - 1, y, direction, 0)
-    // console.log('1 2 line up left', line, up, left)
-    return line === -1 && !isFree({ x, y: y + 1 }) && direction === 1
-      ? 0
-      : line === -1 && !isFree({ x, y: y - 1 }) && direction === 1
-      ? 2
-      : line === -1 && !isFree({ x: x + 1, y }) && direction === 2
-      ? 3
-      : line === -1 && !isFree({ x: x - 1, y }) && direction === 2
-      ? 1
-      : (up === -1 || left === -1) && direction === 1
-      ? 1
-      : (up === -1 || left === -1) && direction === 2
-      ? 2
-      : up === 0 && left === 0
-      ? 'nop'
-      : up === left && direction === 1
-      ? false // all sides occupied 0
-      : up === left && direction === 2
-      ? 3
-      : line > up && line > left && direction === 2
-      ? 2
-      : line > up && line > left && direction === 1
-      ? 1
-      : up > left
-      ? 1
-      : 2
-  }
-}
-const symmetric03 = (x, y, direction) => {
-  if (direction === 3 || direction === 0) {
-    let right = calDistance(x + 1, y, 1, 0)
-    let down = calDistance(x, y + 1, 2, 0)
-    let line =
-      direction === 0
-        ? calDistance(x + 1, y, direction, 0)
-        : calDistance(x, y + 1, direction, 0)
-    // console.log('0 3 down right', line, down, right)
-    return line === -1 && !isFree({ x, y: y + 1 }) && direction === 3
-      ? 0
-      : line === -1 && !isFree({ x, y: y - 1 }) && direction === 3
-      ? 2
-      : line === -1 && !isFree({ x: x + 1, y }) && direction === 0
-      ? 3
-      : line === -1 && !isFree({ x: x - 1, y }) && direction === 0
-      ? 1
-      : (down === -1 || right === -1) && direction === 0
-      ? 0
-      : (down === -1 || right === -1) && direction === 3
-      ? 3
-      : down === 0 && right === 0
-      ? 'nop'
-      : down === right && direction === 3
-      ? false // 2
-      : down === right && direction === 0
-      ? 1
-      : down > right || (line > down && line > right && direction === 3)
-      ? 3
-      : down < right || (line > right && line > down && direction === 0)
-      ? 0
-      : 0
-  }
-}
-const symmetric01 = (x, y, direction) => {
-  if (direction === 0 || direction === 1) {
-    let down = calDistance(x, y + 1, 2, 0)
-    let left = calDistance(x - 1, y, 3, 0)
-    let line =
-      direction === 0
-        ? calDistance(x - 1, y, direction, 0)
-        : calDistance(x, y + 1, direction, 0)
-    return line === -1 && !isFree({ x, y: y + 1 }) && direction === 1
-      ? 0
-      : line === -1 && !isFree({ x, y: y - 1 }) && direction === 1
-      ? 2
-      : line === -1 && !isFree({ x: x + 1, y }) && direction === 0
-      ? 3
-      : line === -1 && !isFree({ x: x - 1, y }) && direction === 0
-      ? 1
-      : (down === -1 || left === -1) && direction === 1
-      ? 1
-      : (down === -1 || left === -1) && direction === 0
-      ? 0
-      : down === 0 && left === 0
-      ? 'nop'
-      : down === left && direction === 1
-      ? false // 2
-      : down === left && direction === 0
-      ? 3
-      : down > left || (line > down && line > left && direction === 1)
-      ? 1
-      : down < left || (line > down && line > left && direction === 0)
-      ? 0
-      : 0
-  }
-}
-const symmetric23 = (x, y, direction) => {
-  if (direction === 2 || direction === 3) {
-    let up = calDistance(x, y - 1, 0, 0)
-    let right = calDistance(x + 1, y, 1, 0)
-    let line =
-      direction === 2
-        ? calDistance(x + 1, y, direction, 0)
-        : calDistance(x, y - 1, direction, 0)
-    // console.log('2 3 line up right', line, up, right)
-    return line === -1 && !isFree({ x, y: y + 1 }) && direction === 3
-      ? 0
-      : line === -1 && !isFree({ x, y: y - 1 }) && direction === 3
-      ? 2
-      : line === -1 && !isFree({ x: x + 1, y }) && direction === 2
-      ? 3
-      : line === -1 && !isFree({ x: x - 1, y }) && direction === 2
-      ? 1
-      : (up === -1 || right === -1) && direction === 3
-      ? 3
-      : (up === -1 || right === -1) && direction === 2
-      ? 2
-      : up === 0 && right === 0
-      ? 'nop'
-      : up === right && direction === 3
-      ? false // 0
-      : up === right && direction === 2
-      ? 1
-      : up > right || (line > up && line > right && direction === 3)
-      ? 3
-      : up < right || (line > right && line > right && direction === 2)
-      ? 2
-      : 2
-  }
-}
-
 const isAlley = ({ x, y }) => !isFree({ x, y }) || !isInBounds({ x, y })
 
 // this functions will find the best path, so the path that has more empty spaces
 // so use `isFree`,
 const findBestPath = (state) => {
   let arr = []
+  let car = state.player.cardinal
+  // if it as a block on the symmetric position it must
+  // simulate the symmetric position and see witch path is the best
+  if (
+    (car === 3 || car === 0) &&
+    !isFree({ x: state.player.x - 1, y: state.player.y - 1 }) &&
+    isFree({ x: state.player.x, y: state.player.y - 1 }) &&
+    isFree({ x: state.player.x - 1, y: state.player.y })
+  ) {
+    let xad = state.player.x - 1
+    let yad = state.player.y - 1
 
-  if (state.player.cardinal === 2 || state.player.cardinal === 3) {
-    let xad = state.player.x - 1
-    let yad = state.player.y + 1
-    const car = symmetric23(xad, yad, state.player.cardinal)
-    if (!isFree({ x: xad, y: yad }) && car !== 'nop') {
-      const index = car === false ? 0 : car
-      return state.player.coords[index]
-    }
+    let choose = [
+      calDistance(xad + 1, yad, 1, 0),
+      calDistance(state.player.x, state.player.y - 1, car, 0),
+      calDistance(state.player.x - 1, state.player.y, car, 0),
+      calDistance(xad, yad + 1, 2, 0),
+    ]
+    let index = choose.indexOf(Math.max(...choose))
+    return index === 0 || index === 1
+      ? state.player.coords[0]
+      : state.player.coords[3]
   }
-  if (state.player.cardinal === 0 || state.player.cardinal === 3) {
-    // if it as a block on the symmetric position it must
-    // simulate the symmetric position and see witch path is the best
-    let xad = state.player.x - 1
-    let yad = state.player.y - 1
-    const car = symmetric03(xad, yad, state.player.cardinal)
-    if (!isFree({ x: xad, y: yad }) && car !== 'nop') {
-      const index = car === false ? 2 : car
-      return state.player.coords[index]
-    }
-  }
-  if (state.player.cardinal === 0 || state.player.cardinal === 1) {
+  if (
+    (car === 1 || car === 0) &&
+    !isFree({ x: state.player.x + 1, y: state.player.y - 1 }) &&
+    isFree({ x: state.player.x, y: state.player.y - 1 }) &&
+    isFree({ x: state.player.x + 1, y: state.player.y })
+  ) {
     let xad = state.player.x + 1
     let yad = state.player.y - 1
-    const car = symmetric01(xad, yad, state.player.cardinal)
-    if (!isFree({ x: xad, y: yad }) && car !== 'nop') {
-      const index = car === false ? 2 : car
-      return state.player.coords[index]
-    }
+    // choose will save the biggest path to be chosen
+    // [ down, line1, line0, left ]
+    let choose = [
+      calDistance(xad, yad + 1, 2, 0),
+      calDistance(state.player.x + 1, state.player.y, car, 0),
+      calDistance(state.player.x, state.player.y - 1, car, 0),
+      calDistance(xad - 1, yad, 3, 0),
+    ]
+    let index = choose.indexOf(Math.max(...choose))
+    return index === 0 || index === 1
+      ? state.player.coords[1]
+      : state.player.coords[0]
   }
-  if (state.player.cardinal === 1 || state.player.cardinal === 2) {
+  if (
+    (car === 2 || car === 1) &&
+    !isFree({ x: state.player.x + 1, y: state.player.y + 1 }) &&
+    isFree({ x: state.player.x, y: state.player.y + 1 }) &&
+    isFree({ x: state.player.x + 1, y: state.player.y })
+  ) {
     let xad = state.player.x + 1
     let yad = state.player.y + 1
-    const car = symmetric12(xad, yad, state.player.cardinal)
-    if (!isFree({ x: xad, y: yad }) && car !== 'nop') {
-      const index = car === false ? 0 : car
-      return state.player.coords[index]
-    }
+    // choose will save the biggest path to be chosen
+    // [ left, line2, line1, up ]
+    let choose = [
+      calDistance(xad - 1, yad, 3, 0),
+      calDistance(state.player.x, state.player.y + 1, car, 0),
+      calDistance(state.player.x + 1, state.player.y, car, 0),
+      calDistance(xad, yad - 1, 0, 0),
+    ]
+    let index = choose.indexOf(Math.max(...choose))
+    return index === 0 || index === 1
+      ? state.player.coords[2]
+      : state.player.coords[1]
+  }
+  if (
+    (car === 2 || car === 3) &&
+    !isFree({ x: state.player.x - 1, y: state.player.y + 1 }) &&
+    isFree({ x: state.player.x, y: state.player.y + 1 }) &&
+    isFree({ x: state.player.x - 1, y: state.player.y })
+  ) {
+    let xad = state.player.x - 1
+    let yad = state.player.y + 1
+    // choose will save the biggest path to be chosen
+    // [ right, line2, line3, up ]
+    let choose = [
+      calDistance(xad + 1, yad, 1, 0),
+      calDistance(state.player.x - 1, state.player.y, car, 0),
+      calDistance(state.player.x, state.player.y + 1, car, 0),
+      calDistance(xad, yad - 1, 0, 0),
+    ]
+    let index = choose.indexOf(Math.max(...choose))
+    return index === 0 || index === 1
+      ? state.player.coords[3]
+      : state.player.coords[2]
   }
 
   for ({ x, y, cardinal } of state.player.coords) {
@@ -209,11 +118,11 @@ const calDistance = (x, y, car, count) => {
     if (
       isFree({ x, y }) &&
       isAlley({ x: x + 1, y }) &&
-      isAlley({ x: x, y: y - 1 }) &&
+      isAlley({ x, y: y - 1 }) &&
       isAlley({ x: x - 1, y })
     )
       return -1
-    return !isFree({ x, y }) || !isFree({ x, y: y - 1 }) || !inBounds(y - 1)
+    return !isFree({ x, y }) || !inBounds(y - 1)
       ? count
       : calDistance(x, y - 1, car, count + 1)
   }
@@ -225,7 +134,7 @@ const calDistance = (x, y, car, count) => {
       isAlley({ x: x + 1, y })
     )
       return -1
-    return !isFree({ x, y }) || !isFree({ x: x + 1, y }) || !inBounds(x + 1)
+    return !isFree({ x, y }) || !inBounds(x + 1)
       ? count
       : calDistance(x + 1, y, car, count + 1)
   }
@@ -237,7 +146,7 @@ const calDistance = (x, y, car, count) => {
       isAlley({ x: x + 1, y })
     )
       return -1
-    return !isFree({ x, y }) || !isFree({ x, y: y + 1 }) || !inBounds(y + 1)
+    return !isFree({ x, y }) || !inBounds(y + 1)
       ? count
       : calDistance(x, y + 1, car, count + 1)
   }
@@ -249,16 +158,13 @@ const calDistance = (x, y, car, count) => {
       isAlley({ x, y: y + 1 })
     )
       return -1
-    return !isFree({ x, y }) || !isFree({ x: x - 1, y }) || !inBounds(x - 1)
+    return !isFree({ x, y }) || !inBounds(x - 1)
       ? count
       : calDistance(x - 1, y, car, count + 1)
   }
 }
 
-const update = (state) => {
-  // return state.players.length <= 2 ? attackMode(state) : findBestPath(state)
-  return findBestPath(state)
-}
+const update = (state) => findBestPath(state)
 
 addEventListener(
   'message',
